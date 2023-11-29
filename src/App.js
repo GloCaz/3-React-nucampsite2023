f58766d672e681699f2acdfecfc768ca392f90bd
Adriana Cazarez <adriana.danzzer@gmail.com>
2023-11-28 23:25:51 -0700

    Week 2 Workshop: added React Components, added routing, new selector

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import AboutPage from './pages/AboutPage';
 import './App.css';
 
 function App() {
@@ -18,8 +19,8 @@ function App() {
                 <Route
                     path='directory/:campsiteId'
                     element={<CampsiteDetailPage />}
-
                 />
+                <Route path='about' element={<AboutPage />} />
             </Routes>
             <Footer />
         </div>
